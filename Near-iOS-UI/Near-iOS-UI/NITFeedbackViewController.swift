78b61a99ef8c1e974984b736c0d69265bfffe7d2
Nicola Ferruzzi <nicola.ferruzzi@gmail.com>
2017-10-16 17:49:30 +0200

    Feedback dialog: wip (assets, basic controller and xib)

---
+++ Near-iOS-UI/Near-iOS-UI/NITFeedbackViewController.swift
@@ -0,0 +1,36 @@
+//
+//  NITFeedbackViewController.swift
+//  NeariOSUI
+//
+//  Created by Nicola Ferruzzi on 16/10/2017.
+//  Copyright © 2017 Near. All rights reserved.
+//
+
+import UIKit
+
+public class NITFeedbackViewController: NITBaseViewController {
+
+    public init() {
+        let bundle = Bundle(for: NITDialogController.self)
+        super.init(nibName: "NITFeedbackViewController", bundle: bundle)
+    }
+
+    public required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    public func show(configureDialog: ((_ dialogController: NITDialogController) -> ())? = nil ) {
+        if let viewController = UIApplication.shared.keyWindow?.currentController() {
+            self.show(fromViewController: viewController, configureDialog: configureDialog)
+        }
+    }
+
+    public func show(fromViewController: UIViewController, configureDialog: ((_ dialogController: NITDialogController) -> ())? = nil) {
+        let dialog = NITDialogController(viewController: self)
+        if let configDlg = configureDialog {
+            configDlg(dialog)
+        }
+        fromViewController.present(dialog, animated: true, completion: nil)
+    }
+
+}
