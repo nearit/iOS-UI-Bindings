9968b98a6b963eb09ec43eb825fbfb0d4cd4a6a8
Nicola Ferruzzi <nicola.ferruzzi@gmail.com>
2017-10-16 19:09:01 +0200

    Feedback dlg: add stars logic and few customization items

--- Near-iOS-UI/Near-iOS-UI/NITFeedbackViewController.swift
+++ Near-iOS-UI/Near-iOS-UI/NITFeedbackViewController.swift
@@ -7,12 +7,27 @@
 //
 
 import UIKit
+import NearITSDK
 
 public class NITFeedbackViewController: NITBaseViewController {
 
-    public init() {
+    public var sendButton: UIImage!
+    public var rateFullButton: UIImage!
+    public var rateEmptyButton: UIImage!
+    public var feedback: NITFeedback!
+
+    @IBOutlet weak var send: UIButton!
+    @IBOutlet weak var comment: UITextView!
+
+    @IBOutlet var stars: [UIButton]!
+
+    @IBOutlet weak var explanation: UILabel!
+
+    public init(feedback: NITFeedback) {
         let bundle = Bundle(for: NITDialogController.self)
         super.init(nibName: "NITFeedbackViewController", bundle: bundle)
+        self.feedback = feedback
+        setupDefaultElements()
     }
 
     public required init?(coder aDecoder: NSCoder) {
@@ -33,4 +48,37 @@ public class NITFeedbackViewController: NITBaseViewController {
         fromViewController.present(dialog, animated: true, completion: nil)
     }
 
+    func setupDefaultElements() {
+        let bundle = Bundle(for: NITDialogController.self)
+        let filledButton = UIImage(named: "filledButton", in: bundle, compatibleWith: nil)
+        sendButton = filledButton?.resizableImage(withCapInsets: UIEdgeInsets(top: 0, left: 45, bottom: 0, right: 45))
+        rateFullButton = UIImage(named: "star", in: bundle, compatibleWith: nil)
+        rateEmptyButton = UIImage(named: "starEmpty", in: bundle, compatibleWith: nil)
+    }
+
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+
+    internal func setupUI() {
+        send.setBackgroundImage(sendButton, for: .normal)
+        comment.layer.cornerRadius = 5.0
+        comment.layer.borderWidth = 1.0
+        comment.layer.borderColor = UIColor.init(white: 225.0/255.0, alpha: 1.0).cgColor
+        for rate in stars {
+            rate.setImage(rateEmptyButton, for: .normal)
+            rate.setImage(rateFullButton, for: .selected)
+        }
+    }
+
+    @IBAction func onStarTouchUpInside(_ sender: UIButton) {
+        var unselected = false
+        for rate in stars {
+            rate.isSelected = !unselected
+            if rate == sender {
+                unselected = true
+            }
+        }
+    }
 }
