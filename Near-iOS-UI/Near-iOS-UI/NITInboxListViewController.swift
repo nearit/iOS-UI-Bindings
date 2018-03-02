fafb9b13a84d2d7026e17b4a704b838ccdc4c6f4
Francesco Leoni <francesco@nearit.com>
2018-03-02 09:46:44 +0100

    Show no contentView in viewDidLoad

--- Near-iOS-UI/Near-iOS-UI/NITInboxListViewController.swift
+++ Near-iOS-UI/Near-iOS-UI/NITInboxListViewController.swift
@@ -63,6 +63,7 @@ public class NITInboxListViewController: NITBaseViewController {
             }
         }
         
+        showNoContentViewIfAvailable()
         setupUI()
         setupDefaultElements()
         refreshInbox()
