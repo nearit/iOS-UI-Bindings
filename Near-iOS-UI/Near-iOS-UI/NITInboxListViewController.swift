e555626f981de5603e7901365ba47b717950cbcb
Cattaneo Stefano <stefano@nearit.com>
2018-05-15 13:36:30 +0200

    feat(trackings): pass trackingInfo from list tap-to-detail

--- Near-iOS-UI/Near-iOS-UI/NITInboxListViewController.swift
+++ Near-iOS-UI/Near-iOS-UI/NITInboxListViewController.swift
@@ -237,7 +237,7 @@ extension NITInboxListViewController: UITableViewDataSource, UITableViewDelegate
                 delegate?.inboxListViewController(self, willShowViewController: feedbackVC)
                 feedbackVC.show(fromViewController: self, configureDialog: nil)
             } else if let content = item.reactionBundle as? NITContent {
-                let contentVC = NITContentViewController(content: content)
+              let contentVC = NITContentViewController(content: content, trackingInfo: item.trackingInfo)
                 delegate?.inboxListViewController(self, willShowViewController: contentVC)
                 contentVC.show(fromViewController: self, configureDialog: nil)
             }
