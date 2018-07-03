b27eb7e3b64fa6eea014d750c21e9489af400bba
Cattaneo Stefano <stefano@nearit.com>
2018-07-03 12:05:33 +0200

    feat(fonts): more global fields and fonts in the sample app

--- UI-Swift-Sample/UI-Swift-Sample/ViewController.swift
+++ UI-Swift-Sample/UI-Swift-Sample/ViewController.swift
@@ -28,6 +28,20 @@ class ViewController: UIViewController {
         
         tableView.rowHeight = UITableViewAutomaticDimension
         tableView.estimatedRowHeight = 60
+
+        for family in UIFont.familyNames.sorted() {
+            let names = UIFont.fontNames(forFamilyName: family)
+            print("Family: \(family) Font names: \(names)")
+        }
+        
+        NITUIAppearance.sharedInstance.regularFontName = "Raleway-Regular"
+        NITUIAppearance.sharedInstance.mediumFontName = "Raleway-Medium"
+        NITUIAppearance.sharedInstance.boldFontName = "Raleway-Bold"
+        NITUIAppearance.sharedInstance.italicFontName = "Raleway-Italic"
+        NITUIAppearance.sharedInstance.mediumItalicFontName = "Raleway-MediumItalic"
+        NITUIAppearance.sharedInstance.boldItalicFontName = "Raleway-BoldItalic"
+        
+        // Raleway-Regular
     }
 
     override func didReceiveMemoryWarning() {
